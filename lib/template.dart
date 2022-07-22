477c2d4f3287c6376b34264cd84ad72807c7ee03
Apridev <apriantofajar11@gmail.com>
2022-07-22 14:43:36 +0900

    template

---
+++ lib/template.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = const Color(0xff1EB2A6);
+Color blackColor = const Color(0xff303841);
+Color blackColor2 = const Color(0xff495464);
+Color backgroundColor = const Color(0xffffffff);
+Color inputBorderColor = const Color(0xffECECEC);
+Color redNotifColor = const Color(0xffF37065);
+Color yellowStar = const Color(0xffFBBC05);
+
+TextStyle primaryFontStyle = GoogleFonts.montserrat(
+  color:  primaryColor
+);
+
+TextStyle blackFontStyle = GoogleFonts.montserrat(
+  color:  blackColor
+);
+
+TextStyle blackFontStyle2 = GoogleFonts.montserrat(
+  color:  blackColor2
+);
+
+TextStyle backgroundFontStyle = GoogleFonts.montserrat(
+  color:  backgroundColor
+);
+
+TextStyle redNotifFontStyle = GoogleFonts.montserrat(
+  color:  redNotifColor
+);
+
+TextStyle yellowStaFontStyle = GoogleFonts.montserrat(
+  color:  yellowStar
+);
+
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
